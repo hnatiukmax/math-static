09a691f4001cd3a29deb0c1a47ef731ae0a47aea
sectumsempra30 <m.gnatyuk16@gmail.com>
2019-04-22 12:14:37 +0300

    adding example with lambda

--- src/main/main.kt
+++ src/main/main.kt
@@ -3,5 +3,15 @@ package main
 import math.*
 
 fun main(args: Array<String>) {
-    print("Hello, Git!!!")
+    //lambda 1
+    val function = MULT(CONST(4.0), ARG())
+    val exp1 : (Double) -> Double  =  { value ->  function.Calculate(value) }
+
+    //lambda 2
+    val exp2 = { value : Double -> value*4 + Math.pow(value, 3.0) }
+
+    println("Value of Integrate exp1 = " +
+            "${Numerical_Methods.Integrate(exp1, 0.0, 4.0, IntegrateType.TRAPEZE)}")
+    println("Value of Integrate exp2 = " +
+            "${Numerical_Methods.Integrate(exp2, 0.0, 4.0, IntegrateType.TRAPEZE)}")
 }
